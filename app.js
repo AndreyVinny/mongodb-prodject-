a7b25da8ba54a240e9a3690e9d01e8d45a1bde97
Andrey <noonverto007@gmail.com>
2022-08-31 11:15:10 +0300

    add .env

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const express =require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {DB_HOST} = process.env;
+console.log(DB_HOST);
 
 const app = express();
 app.use(cors());   // возврат кром доменных запросво
@@ -9,7 +14,6 @@ app.get('/', (req, res) => {
     res.send("<h2>Home page</h2>")
 });
 
-const DB_HOST = "mongodb+srv://Andrew:jasgwDWBguu7uf0g@cluster0.0uadonb.mongodb.net/online_shop?retryWrites=true&w=majority";
 
 mongoose.connect(DB_HOST)
     .then(()=> {
